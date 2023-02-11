35059169ded2bb702d9ec6c65350dfab9f2f1212
jackfusion <kendewitt@yftg.ca>
2023-02-11 16:16:17 -0600

    adding sendgrid

--- .history/pages/api/mail_20230211160735.js
+++ .history/pages/api/mail_20230211160735.js
@@ -1,7 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const mail = require('@sendgrid/mail');
 export default function handler(req, res) {
-  const body = JSON.parse(req.body);
-  mail.setApiKey(process.env.SENDGRID_API_KEY);
   res.status(200).json({ status: 'OK' })
 }
