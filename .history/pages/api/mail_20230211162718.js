ba4980428f91548f2c341554f25e293a9252ef2e
jackfusion <kendewitt@yftg.ca>
2023-02-11 16:32:22 -0600

    finish setting up send.mail fusntion

--- .history/pages/api/mail_20230211162718.js
+++ .history/pages/api/mail_20230211162718.js
@@ -11,11 +11,13 @@ export default function handler(req, res) {
   mail.setApiKey(process.env.SENDGRID_API_KEY);
 
   mail.send({
-    to: 'kendewitt@yftg.ca',
-    from: 'kendewitt@yftg.ca',
-    subject: 'New Message from Contact form',
-    text: message,
-    html: message.replace(/rn/g, '<br>'),
-  }).then(() => {
-    res.status(200).json({ status: 'OK' });
+  to: 'to.name@email.com',
+  from: 'from.name@email.com',
+  subject: 'New Message!',
+  text: message,
+  html: message.replace(/rn/g, '<br>'),
   });
+
+  res.status(200).json({ status: 'OK' })
+  console.log('body', body);
+}
